19172189cd2b7a429b93f70f2b8fe611824a060b
JuaniF <juanignaciof86@gmail.com>
2022-09-06 23:21:43 -0300

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <>
+            <img src='' alt=''></img>
+            <h1>Título</h1>
+            <nav>
+                <a>Home</a>
+                <a>Nos</a>
+                <a>Contacto</a>
+            </nav>
+        </>
+    )
+}
+
+export default Header
+
